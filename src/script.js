503f3576e6b6fb263681500fe6abfe1f088eb244
André Vega <ventaslederer@hotmail.com>
2021-12-01 13:22:05 -0600

    Working on the implementaion of add Event Listeners

--- src/script.js
+++ src/script.js
@@ -1,2 +1,30 @@
-var mpButton = document.querySelector(".main-page__button")
-console.log(mpButton.textContent)
+
+var score;
+
+// var answerButtons = document.querySelectorAll('button[data-answer]');
+var allSections = document.querySelector(".all-pages__section")
+var questionSections = document.querySelector('.all-questions__sections')
+
+var scoreValue = document.querySelector(".score__span")
+//     console.log(scoreValue)
+
+//Function for all event listeners
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if( e.target.matches(selector)) callback(e)
+    })
+}
+
+// Event Listeners
+addGlobalEventListener("click","button", e => {
+    console.log(e.target.dataset.answer)
+    if (e.target.dataset.answer === "true") {
+        score += 20
+        console.log("correcto!")
+        console.log(score)
+        return
+    }
+    if (e.target.classlist = "main-page__button")
+        console.log(scoreValue)
+        return
+} );
