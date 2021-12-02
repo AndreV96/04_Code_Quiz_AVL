2943f829976a116ca355b0bab08254b7953d222f
André Vega <ventaslederer@hotmail.com>
2021-12-01 19:04:28 -0600

    Made Starquiz button with function that loads info and first quesation, added tiemer and score, working on fucntions that loads next possible answers

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Module 4
+
+Webpage Deployed link: 
+
+GitHub Repository Link: https://github.com/AndreV96/04_Code_Quiz_AVL.git
--- src/script.js
+++ src/script.js
@@ -1,12 +1,26 @@
 
-var score;
+var score = 0;
+var time = 76;
+var questionNumber;
+var question = ["Commonly used data types DO NOT include:", "The condition of an if/else statement is enclosed within ____.", "Arrays in Javascript can be used to store ____.", "String values must be enclosed within ____ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to be debugged is:" ] 
+var answers1 = ["strings", "booleans", "alerts", "numbers"]
+var answers2 = ["quotes", "curly brackets", "parenthesis", "square brackets"]
+var answers3 = ["numbers and strings", "other arrays", "booleans", "all of the above"]
+var answers4 = ["commas", "curly brackets", "quotes", "parenthesis"]
+var answers5 = ["JavaScript", "terminal/bash", "for loops", "console.log"]
 
 // var answerButtons = document.querySelectorAll('button[data-answer]');
 var allSections = document.querySelector(".all-pages__section")
 var questionSections = document.querySelector('.all-questions__sections')
+var startQuizSection = document.querySelector(".start-quiz__section")
+var questionSection = document.querySelector(".question__section")
 
+var questionH1 = document.querySelector(".question__h1")
+
+var scoreP = document.querySelector(".score__p")
 var scoreValue = document.querySelector(".score__span")
-//     console.log(scoreValue)
+var timeP = document.querySelector(".time__p")
+var timeValue = document.querySelector(".time__span")
 
 //Function for all event listeners
 function addGlobalEventListener(type, selector, callback) {
@@ -14,17 +28,57 @@ function addGlobalEventListener(type, selector, callback) {
         if( e.target.matches(selector)) callback(e)
     })
 }
+function loadNextQuestion() {
+    if (questionNumber < 5) {
+        questionNumber += 1
+        questionH1.innerHTML = question[questionNumber]
 
+    } 
+}
+//Main functions
+function startQuiz() {
+    startQuizSection.classList.add("display-none");
+    questionSection.classList.remove("display-none");
+    scoreP.classList.remove("display-none")
+    scoreValue.classList.remove("display-none")
+    timeP.classList.remove("display-none")
+    questionNumber = 1;
+    timer()
+    
+console.log(questionH1.innerHTML)
+}
+function timer() {
+    time = time -1;
+    timeValue.innerHTML = time;
+    setTimeout(timer, 1000);
+}
+
+// function noTimeLeft() {
+
+// } 
 // Event Listeners
 addGlobalEventListener("click","button", e => {
-    console.log(e.target.dataset.answer)
-    if (e.target.dataset.answer === "true") {
-        score += 20
-        console.log("correcto!")
-        console.log(score)
+    var eClass = e.target.getAttribute("class")
+    var eDataAnswer = e.target.getAttribute("data-answer")
+    console.log(eDataAnswer)
+    console.log(eClass)
+    e.stopPropagation()
+    if (eClass.includes("start-quiz__button") === true) {
+        startQuiz()
+        console.log("inicio quiz")
         return
     }
-    if (e.target.classlist = "main-page__button")
-        console.log(scoreValue)
+    if (eDataAnswer === "true") {
+        score += 20
+        scoreValue.innerHTML = score
+        loadNextQuestion();
+        console.log("correcto!")
         return
+    }
+    if (eDataAnswer === "false") {
+        time -= 15
+        loadNextQuestion();
+        console.log("incorrecto!")
+        return
+    }
 } );
